2700be701cd36b5087d63a0213756002a83c2b71
chriskulbaba2025 <chris@rgdirect.com>
2025-10-16 13:03:03 -0400

    Clean next.config.ts merge markers removed

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,15 @@
-<<<<<<< HEAD
-// next.config.js
-import type { NextConfig } from 'next';
-import path from 'path';
-=======
-﻿import type { NextConfig } from "next";
->>>>>>> 79875a4 (Update Navbar and tiles to link to brand voice)
+// next.config.ts
+import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
     remotePatterns: [
-<<<<<<< HEAD
       {
-        protocol: 'https',
-        hostname: 'omnipressence.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "omnipressence.com",
+        pathname: "/wp-content/**",
       },
     ],
   },
@@ -25,16 +19,11 @@ const nextConfig: NextConfig = {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      include: [path.resolve(__dirname, 'src/icons')],
-      use: ['@svgr/webpack'],
+      include: [path.resolve(__dirname, "src/icons")],
+      use: ["@svgr/webpack"],
     });
     return config;
   },
-=======
-      { protocol: "https", hostname: "omnipressence.com", pathname: "/wp-content/**" },
-    ],
-  },
->>>>>>> 79875a4 (Update Navbar and tiles to link to brand voice)
 };
 
 export default nextConfig;
