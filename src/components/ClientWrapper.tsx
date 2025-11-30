b120c3ab599e880dcffe05dd73d549ebe5982987
Chris Kulbaba <chris@omnipressence.com>
2025-11-29 19:17:50 -0500

    replaced client wrapper

--- src/components/ClientWrapper.tsx
+++ src/components/ClientWrapper.tsx
@@ -1,17 +1,8 @@
-"use client";
+'use client';
 
-import React from "react";
+import { ReactNode } from 'react';
+import { UserProvider } from '@/context/UserContext';
 
-export default function ClientWrapper({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <div className="min-h-screen w-full bg-[#f8fafc] dark:bg-[#101010] flex justify-center">
-      <div className="w-full max-w-[1400px] px-[40px] py-[20px]">
-        {children}
-      </div>
-    </div>
-  );
+export default function ClientWrapper({ children }: { children: ReactNode }) {
+  return <UserProvider>{children}</UserProvider>;
 }
