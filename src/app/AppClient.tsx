33b96b857b86759f74c1ee0205cd6b4b099d4dfc
Chris Kulbaba <chris@omnipressence.com>
2025-11-30 22:27:03 -0500

    remove ConditionalLayout wrapper from AppClient component

--- src/app/AppClient.tsx
+++ src/app/AppClient.tsx
@@ -1,7 +1,5 @@
 "use client";
-import { ConditionalLayout } from "@/components/ConditionalLayout";
 
 export default function AppClient({ children }: { children: React.ReactNode }) {
-  return <ConditionalLayout>{children}</ConditionalLayout>;
+  return <>{children}</>;
 }
-
