0408d54691f2e4d2ef149b76fbe581e3c8250dde
Chris Kulbaba <chris@omnipressence.com>
2025-12-02 20:21:35 -0500

    feat: add SpokeDetailPage component for displaying spoke details

--- src/app/spoke/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function SpokeIndexPage() {
-  return (
-    <main className="p-10 text-center">
-      <h1 className="text-2xl font-semibold text-[#10284a]">
-        Select a hub from the wheel to continue.
-      </h1>
-    </main>
-  );
-}
